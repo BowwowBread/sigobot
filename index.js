968266a2fc4474940ae126767b6f45fcb3f7853d
bowwow <bowwow.bread@gmail.com>
2017-04-14 19:49:11 +0900

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request')
+const app = express()
+
+const token = process.env.FB_VERIFY_TOKEN
+const access = process.env.FB_ACCESS_TOKEN
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.send('this is sigo chatbot!')
+})
+
+app.get('/webhook/', function(req, res) {
+  if(req.query['hub.verify_token'] === token) {
+    res.send(req.query['hub.challenge'])
+   }
+   res.send('No entry')
+})
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  if (data.object === 'page') {
+
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    res.sendStatus(200);
+  }
+});
+  
+function receivedMessage(event) {
+  console.log("Message data: ", event.message);
+}
+
+app.listen(app.get('port'), function() {
+  console.log('running on port', app.get('port'))
+})
