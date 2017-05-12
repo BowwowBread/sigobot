92c74163ce5952861dfde45e8713176ee28826d2
bowwow <bowwow.bread@gmail.com>
2017-05-12 13:49:15 +0900

    remove sdk init

--- index.js
+++ index.js
@@ -8,29 +8,6 @@ const cheerio = require('cheerio');
 const token = process.env.FB_VERIFY_TOKEN
 const access = process.env.FB_ACCESS_TOKEN
 
-/**
- * facebook SDK 
- */
-// window.fbAsyncInit = function () {
-//   FB.init({
-//     appId: '499867256804339',
-//     xfbml: true,
-//     version: 'v2.9'
-//   });
-//   FB.AppEvents.logPageView();
-// };
-
-// (function (d, s, id) {
-//   var js, fjs = d.getElementsByTagName(s)[0];
-//   if (d.getElementById(id)) {
-//     return;
-//   }
-//   js = d.createElement(s);
-//   js.id = id;
-//   js.src = "//connect.facebook.net/en_US/sdk.js";
-//   fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
-
 app.set('port', (process.env.PORT || 5000))
 
 app.use(bodyParser.urlencoded({
