5ccff6295d58a29b0b70e833180e578624b2d2e6
bowwow <bowwow.bread@gmail.com>
2017-06-08 13:41:24 +0900

    port

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const request = require('request');
 const app = express();
 const stringSimilarity = require('string-similarity');
 const cheerio = require('cheerio');
-var https = require('https');
 
 const token = process.env.FB_VERIFY_TOKEN
 const access = "EAAHGoGpG0ZCMBACj6JvTDXPeG949bFTXZCVdYhJsK7Bm1N3GVdBUhjcOsFsUl11eZANgSOEi7FJWFV23ZAB9uIBG7GoLPi6ORIBha0E9fUfWOPsLb3kMKhlRwXtNbsFCsUyI4ZBmbWrQm1VCxPafiKZBLZCBzQSTRyjA1eoLVrxKQZDZD"
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 9990))
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -457,10 +456,6 @@ function callSendAPI(messageData) {
     }
   });
 }
-// app.listen(app.get('port'), function () {
-//   console.log('running on port', app.get('port'))
-// })
-
-var httpsServer = https.createServer(app);
-
-httpsServer.listen(app.get('port'));
+app.listen(app.get('port'), function () {
+  console.log('running on port', app.get('port'))
+})
